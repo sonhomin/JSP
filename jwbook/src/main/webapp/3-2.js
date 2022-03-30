8c6babe48464981d66a9598cada8b6473013fa76
kim-seungmin <ksm981204@naver.com>
2022-03-30 13:33:47 +0900

    week4

---
+++ jwbook/src/main/webapp/3-2.js
@@ -0,0 +1,33 @@
+// a = 5;
+// console.log(a);
+// var a;
+var a = "var test 1";
+console.log(a);
+
+var a = "var test 2";
+console.log(a);
+		
+let b = "let test";
+console.log(b);
+b= "let change";
+console.log(b);
+//let b = "let test"; ERR
+
+const c = "const test c";
+console.log(c);
+//c = "const test 1"; ERR
+
+Human(citizen);
+
+let citizen = "James";
+
+function Human(name) {
+  console.log("Name is: " + name);
+}
+
+
+
+const PI = 3.14;
+const PItoString = String(PI);
+const PItoInt = parseInt(PI);
+console.log(PI+" "+PItoString+" "+PItoInt);
