ed64980e8e5f3128631334968c56db4f45fd0c60
root <root@ksmLab.localdomain>
2022-04-08 16:11:04 +0900

    add 5-2, 6-1

--- jwbook/src/main/java/ch05/CalcServlet.java
+++ jwbook/src/main/java/ch05/CalcServlet.java
@@ -1,5 +1,45 @@
 package ch05;
 
-public class CalcServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+//@WebServlet("/calc")
+public class CalcServlet extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+
+    public CalcServlet() {
+       super();
+    }
+
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      int n1 = Integer.parseInt(request.getParameter("n1"));      
+      String op = request.getParameter("op");
+      
+      response.setContentType("text/html; charset=utf-8");
+      PrintWriter out = response.getWriter();
+      
+      out.append("<html><body><h2>계산기 서블릿</h2><hr> 계산결과: <br>");
+      switch(op){      	
+         case "+": for(int i=1;i<=9;i++) {
+        	 			out.append(n1+"+"+i+"="+(n1+i)+"<br>");
+         			}break;
+        	 
+         case "*": for(int i=1;i<=9;i++) {
+        	 			out.append(n1+"*"+i+"="+(n1*i)+"<br>");
+         			}break;
+      }
+      out.append("</body></html>");      
+   }
+
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      doGet(request, response);
+   }
 }
---
+++ jwbook/src/main/webapp/ch06/ListItem.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<%@ taglib tagdir="/WEB-INF/tags" prefix="my tag" %>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>태그실습</title>
+</head>
+<body>
+<h2>태그실습</h2>
+<HR>
+<mytag:item border="10" bgcolor="yellow">재고현황</mytag:item>
+</body>
+</html>
