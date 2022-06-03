724eba6335d83cf2a7f6750528decf5cfde04501
kim-sungmin <ksm981204@naver.com>
2022-06-03 14:50:32 +0900

    add ConttentsDAO

--- jwbook/src/main/java/whants/ContentsDAO.java
+++ jwbook/src/main/java/whants/ContentsDAO.java
@@ -13,86 +13,94 @@ import whants.Contents;
 public class ContentsDAO {
 	final String JDBC_DRIVER = "org.h2.Driver";
 	final String JDBC_URL = "jdbc:h2:tcp://localhost/~/whants";
-	//DB 연결 가져오는 메서드
+	
 	public Connection open() {
 		Connection conn = null;
 		try {
-		Class.forName(JDBC_DRIVER);
-		conn = DriverManager.getConnection(JDBC_URL,"sa","1234");
-	} catch (Exception e) {
-		e.printStackTrace();
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(JDBC_URL,"sa","1234");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
 	}
-	return conn;
-}
 
- public List<Contents> getAll() throws Exception{
-	Connection conn = open();
-	List<Contents> contentList = new ArrayList<>();
+	public List<Contents> getAll() throws Exception{
+		Connection conn = open();
+		List<Contents> contentList = new ArrayList<>();
 	
-	String sql = "select ContentID, Title, View, PARSEDATETIME(StartDate,'yyyy-MM-dd hh:mm:ss') as sdate, PARSEDATETIME(EndDate,'yyyy-MM-dd hh:mm:ss') as edate, Director, Actor, img from Contents";
-	PreparedStatement pstmt = conn.prepareStatement(sql);
-	ResultSet rs = pstmt.executeQuery();
+		String sql = "select ContentID, Title, View, PARSEDATETIME(StartDate,'yyyy-MM-dd hh:mm:ss') as sdate, PARSEDATETIME(EndDate,'yyyy-MM-dd hh:mm:ss') as edate, Director, Actor, img from Contents";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		ResultSet rs = pstmt.executeQuery();
 	 
-	try(conn; pstmt; rs){
-		while(rs.next()) {
-			Contents c = new Contents();
-			c.setContentID(rs.getInt("ContentID"));
-			c.setTitle(rs.getString("Title"));
+		try(conn; pstmt; rs){
+			while(rs.next()) {
+				Contents c = new Contents();
+				c.setContentID(rs.getInt("ContentID"));
+				c.setTitle(rs.getString("Title"));
+				c.setView(rs.getInt("View"));
+				c.setStartDate(rs.getString("sdate"));
+				c.setEndDate(rs.getString("edate"));
+				c.setDirector(rs.getString("Director"));
+				c.setActor(rs.getString("Actor"));
+				c.setImg(rs.getString("img"));
+				contentList.add(c);
+			}
+			return contentList;
+		}
+	} 	
+ 
+  	public Contents getContents(int ContentID) throws SQLException{
+  		Connection conn = open();
+  		Contents c = new Contents();
+  		String sql = "select ContentID, Title, View, PARSEDATETIME(StartDate,'yyyy-MM-dd hh:mm:ss') as sdate, PARSEDATETIME(EndDate,'yyyy-MM-dd hh:mm:ss') as edate, Director, Actor, img from Contents where ContentID=?";
+  		PreparedStatement pstmt = conn.prepareStatement(sql);
+  		pstmt.setInt(1,  ContentID);
+  		ResultSet rs = pstmt.executeQuery();
+  		rs.next();
+	  
+  		try(conn; pstmt; rs){
+  			c.setContentID(rs.getInt("ContentID"));
+		  	c.setTitle(rs.getString("Title"));
 			c.setView(rs.getInt("View"));
 			c.setStartDate(rs.getString("sdate"));
 			c.setEndDate(rs.getString("edate"));
 			c.setDirector(rs.getString("Director"));
 			c.setActor(rs.getString("Actor"));
 			c.setImg(rs.getString("img"));
-			contentList.add(c);
-			}
-		return contentList;
-		}
-	}
- 
- 	
- //------------------------------------------------------------------------------------------------------------------------------------------------------
-  public Contents getContents(String UserID) throws SQLException{
-	  Connection conn = open();
-	  Contents m = new Contents();
-	  String sql = "select UserID, Passworld, NickName, admin from Contents where UserID=?";
-	  PreparedStatement pstmt = conn.prepareStatement(sql);
-	  pstmt.setString(1,  UserID);
-	  ResultSet rs = pstmt.executeQuery();
-	  rs.next();
-	  
-	  try(conn; pstmt; rs){
-		m.setUserID(rs.getString("UserID"));
-		m.setPassworld(rs.getString("Passworld"));
-		m.setNickName(rs.getString("NickName"));
 		pstmt.executeQuery();
-		return m;
-	  }
-  }
+		return c;
+  		}
+  	}
  
-  public void addContents(Contents m) throws Exception{
-	  Connection conn = open();
-	  String sql = "insert into Contents(UserID, Passworld, NickName, admin) values(?,?,?,0)";
-	  PreparedStatement pstmt = conn.prepareStatement(sql);
+  	public void addContents(Contents c) throws Exception{
+  		Connection conn = open();
+  		String sql = "insert into Contents (ContentID, Title, View, StartDate, EndDate, Director, Actor, img) values(?,?,?,?,?,?,?,?)";
+  		PreparedStatement pstmt = conn.prepareStatement(sql);
 	  
-	  try(conn; pstmt){
-		  pstmt.setString(1, m.getUserID());
-		  pstmt.setString(2, m.getPassworld());
-		  pstmt.setString(3, m.getNickName());
-		  pstmt.executeUpdate();
-	  }
-  }
+  		try(conn; pstmt){
+  			pstmt.setInt(1, c.getContentID());
+  			pstmt.setString(2, c.getTitle());
+  			pstmt.setInt(3, c.getView());
+  			pstmt.setString(4, c.getStartDate());
+  			pstmt.setString(5, c.getEndDate());
+  			pstmt.setString(6, c.getDirector());
+  			pstmt.setString(7, c.getActor());
+  			pstmt.setString(8, c.getImg());
+  			pstmt.executeUpdate();
+  		}
+  	}
   
-  public void delContents(String Userid) throws SQLException{
-	  Connection conn = open();
-	  String sql = "delete from Contents where Userid=?";
-	  PreparedStatement pstmt = conn.prepareStatement(sql);
+  	public void delContents(String ContentID) throws SQLException{
+  		Connection conn = open();
+  		String sql = "delete from Contents where ContentID=?";
+  		PreparedStatement pstmt = conn.prepareStatement(sql);
 	  
-	  try(conn;pstmt){
-		  pstmt.setString(1, Userid);
-		  if(pstmt.executeUpdate() == 0) {
-			  throw new SQLException("DB에러");
-		  }
-	  }
-  }
+  		try(conn;pstmt){
+  			pstmt.setString(1, ContentID);
+  			if(pstmt.executeUpdate() == 0) {
+  				throw new SQLException("DB에러");
+  			}
+  		}
+  	}
 }
